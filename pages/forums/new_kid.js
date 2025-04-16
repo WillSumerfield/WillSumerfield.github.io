b65183dbc567dc1f3de86b175af98edfe9c797f7
Will Sumerfield <will.sumerfield24@imperial.ac.uk>
2025-04-16 21:34:26 +0100

    new kid updated

--- pages/forums/new_kid.js
+++ pages/forums/new_kid.js
@@ -2,7 +2,7 @@ import { Forum } from '../../assets/js/forum.js';
 import { Post } from '../../assets/js/post.js';
 
 const posts = [
-  new Post("wbc", "08/17/02 10:34 PM", `So I was at the Q-Mart over on 5th earlier, and some punk tried to sneak some merch out. Wasn't too sneaky about it, and the security was after him. Still woulda gotten away with it, but then this kid (like, barely a teenager) just grabs his arm and locks him down. Like, dude was flailing and trying to yank free, but nope, hard stop. Security came over, took the guy in, and the kid just mumbled something and peaced out before anyone could ask his name.
+  new Post("wbc", "08/17/02 10:34 PM", `So I was at the Q-Mart over on 5th earlier, and some punk tried to sneak some merch out. Wasn't too sneaky about it, and the security was after him. Still woulda gotten away with it, but then this kid (like, barely a teenager) just grabs his arm and locks him down. Like, dude was flailing and trying to yank free, but nope, hard stop - kid's arms musta been steel. Security came over, took the guy in, and the kid just mumbled something and peaced out before anyone could ask his name.
 
 Anyone know who he is? New cape? Some random unregged [UC]? Kid was strong, at least.
 Wearing normal clothes - a tee and shorts. No mask or anything. Sandy hair.`),
@@ -11,9 +11,9 @@ Wearing normal clothes - a tee and shorts. No mask or anything. Sandy hair.`),
 What's his power again?`),
   new Post("nav", "08/17/02 10:50 PM", `Nah, he’s back, was just rolling solo till now. Got that weight-lock power—stands still too long and turns into a walking wrecking ball. He was trying to bust into a pawn shop, and the cops couldn’t do jack (LOL). Dude was literally walking through their barricade.
 
-Then this kid runs in, grabs his arm, and Jack just stalls out. Kept trying to push forward, digging his heels in, but the kid held his ground. He kept leaning into it, but the kid didn’t budge, and eventually, the cops got in close enough to dart him.
+Then this kid runs in, grabs his arm, and Jack just stalls out. Kept trying to push forward, digging his heels in, but the kid held his ground. He kept leaning into it, but the kid leaned back - didn't budge an inch. Eventually the cops got in close enough to dart Jack.
 
-Also, idk if it was just the video, but the kid’s arm stretched a little when he grabbed Jack. Not full noodle mode, but something.I think he’s got some kind of super strength. Heard he was a [UC] before, but I dunno if that’s true. He’s been around for a while, though. Just never really did anything until now.`),
+Also, idk if it was just the video, but it looked like kid’s arm stretched a little when he grabbed Jack. Not full noodle mode, but something. Heard he was a [UC] before, but I dunno if that’s true.`),
   new Post("wbc", "08/17/02 10:57 PM", `Lmfao, so this kid just outmuscled Concrete Jack? And might be stretchy?? That’s some S-tier energy for a UC right there.
 Wonder why he’s dipping so fast, though. No quip, no posing, no name drop, just poof.`),
   new Post("cf2", "08/17/02 11:05 PM", `Could be he’s just starting out. No costume, no name, maybe doesn’t even know what he’s doing yet. If he’s really that young, he might not even be a proper hero—just a kid with powers testing the waters.
